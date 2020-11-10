d14cd81872c923ef1cc8016e3e8a2154a428c349
Ramen-Nood1ez <55363734+Ramen-Nood1ez@users.noreply.github.com>
2020-11-10 15:44:31 -0600

    Programmed snake some more

--- Snake/snake.js
+++ Snake/snake.js
@@ -6,25 +6,38 @@ const board_background = "white";
 const snake_col = "lightblue";
 const snake_border = "darkblue";
 
-let snake = [ {x: 200, y: 200}, {x: 190, y: 200},
-  {x: 180, y: 200}, {x: 170, y: 200}, {x: 160, y:200},];
+let snake = [
+  {x: 200, y: 200},
+  {x: 190, y: 200},
+  {x: 180, y: 200},
+  {x: 170, y: 200},
+  {x: 160, y: 200},
+]
+
+let score = 0;
 
 let changing_direction = false;
+
+let food_x;
+let food_y;
 // Horizontal velocity
 let dx = 10;
 // Vertical velocity
 let dy = 0;
 
-main();
-
 document.addEventListener("keydown", change_direction);
+
+main();
+gen_food();
 
 function main() {
   if (has_game_ended()) return;
 
   changing_direction = false;
+
   setTimeout(function onTick(){
     clearCanvas();
+    drawFood();
     move_snake();
     drawSnake();
     // Call main again
@@ -53,7 +66,16 @@ function drawSnake() {
 function move_snake() {
   const head = {x: snake[0].x + dx, y: snake[0].y + dy};
   snake.unshift(head);
-  snake.pop();
+  const has_eaten_food = snake[0].x === food_x && snake[0].y === food_y;
+  if (has_eaten_food) {
+    // Increase score
+    score += 10;
+    // Display score on screen
+    document.getElementById("score").innerHTML = score;
+    gen_food();
+  } else {
+    snake.pop();
+  }
 }
 
 function change_direction(event) {
@@ -62,7 +84,9 @@ function change_direction(event) {
   const UP_KEY = 38;
   const DOWN_KEY = 40;
 
-  if (changing_direction) return changing_direction = true;
+  if (changing_direction) return;
+
+  changing_direction = true;
 
   const keyPressed = event.keyCode;
   const goingUp = dy == -10;
@@ -97,8 +121,30 @@ function has_game_ended() {
   }
   const hitLeftWall = snake[0].x < 0;
   const hitRightWall = snake[0].x > snakeboard.width - 10;
-  const hitTopWall = snake[0].y &lt; 0;
+  const hitTopWall = snake[0].y < 0;
   const hitBottomWall = snake[0].y > snakeboard.height - 10;
 
   return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall
+}
+
+function random_food(min, max) {
+  return Math.round((Math.random() * (max-min) / 10))*10;
+}
+
+function gen_food() {
+  food_x = random_food(0, snakeboard.width - 10);
+  food_y = random_food(0, snakeboard.height - 10);
+  snake.foreach(function has_snake_eaten_food(part) {
+    const has_eaten = part.x == food_x && part.y == food_y;
+    if (has_eaten) {
+      gen_food();
+    }
+  });
+}
+
+function drawFood() {
+  snakeboard_ctx.fillStyle = "lightgreen";
+  snakeboard_ctx.strokeStyle = "darkgreen";
+  snakeboard_ctx.fillRect(food_x, food_y, 10, 10);
+  snakeboard_ctx.strokeRect(food_x, food_y, 10, 10);
 }
